269b6e7c902fa733c83c9e356176372d187d11ab
Jesse Cheung <18372099+dob-j@users.noreply.github.com>
2020-09-24 18:57:16 -0700

    added netlify cms

--- next.config.js
+++ next.config.js
@@ -1,2 +1,20 @@
+const withPlugins = require("next-compose-plugins");
 const withImages = require('next-images')
-module.exports = withImages()
+
+const nextConfig = {
+    webpack: (cfg) => {
+        cfg.module.rules.push(
+            {
+                test: /\.md$/,
+                loader: 'frontmatter-markdown-loader',
+                options: { mode: ['react-component'] }
+            }
+        )
+        return cfg;
+    }
+}
+
+
+module.exports = withPlugins([
+    [withImages],
+], nextConfig);
