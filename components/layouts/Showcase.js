f4af2eb63974b4df443b2ed539f3138d67276804
Jesse Cheung <18372099+dob-j@users.noreply.github.com>
2020-10-17 16:59:50 -0700

    worked on showcase rep component

--- components/layouts/Showcase.js
+++ components/layouts/Showcase.js
@@ -6,50 +6,30 @@ const QBDDonorRank = require('../../assets/userreviews/qbd-donorrank.png');
 const AssassinName = require('../../assets/userreviews/assassin-name.png');
 const AssassinDonorRank = require('../../assets/userreviews/assassin-donorrank.png');
 
-import { Card } from 'react-bootstrap'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 
 import { attributes } from '../../content/showcasecomponent.md'
 
 export default function Showcase() {
-    const { title } = attributes;
+    const { title, card } = attributes;
     return (
-        <div className="container text-center p-1 my-reviewsection">
-            <h4>{title}</h4>
-            <div className="row">
-                    <div className="col-md-4 my-column">
+        <Container style={{backgroundColor: 'orange'}}>
+            <center><h4>{title}</h4></center>
+            <Row>
+                {card.map((singlecard) => (
+                    <Col md={4}>
                         <p>             
-                            <a target="_blank" rel="noopener noreferrer" href="https://www.sythe.org/threads/fyrixs-vouch-thread/#post-30492611">
-                                <img src={SearchyName} alt="SearchyReview" /><img src={SearchyDonorRank} alt="SearchyDonorRank" />
+                            <a target="_blank" rel="noopener noreferrer" href={singlecard.url}>
+                                <img src={singlecard.name} alt={singlecard.namealttext} /><img src={singlecard.donorrank} alt={singlecard.donorrankalttext} />
                             </a>
                             <Card>
-                                <Card.Body>"Huge vouch for this man, bought 6b+ 07 off him over the course of a few months. 
-                                    Always friendly and easy to deal with,
-                                        much appreciated!" (May 6, 2018)</Card.Body>
+                                <Card.Body>{singlecard.message}
+                                </Card.Body>
                             </Card>               
                         </p>
-                    </div>
-                    <div className="col-md-4 my-column">
-                    <p>             
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.sythe.org/threads/fyrixs-vouch-thread/#post-30769035">
-                            <img src={QBDName} alt="QBDReview" /><img src={QBDDonorRank} alt="QBDDonorRank" />
-                        </a>                              
-                        <Card>
-                            <Card.Body>"sold me a WHOPPING 1.3b osrs, excellent trading practices, 
-                                tyvm for choosing me :D" (May 23, 2018)</Card.Body>
-                        </Card>
-                        </p>
-                    </div>
-                    <div className="col-md-4 my-column">
-                    <p>             
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.sythe.org/threads/fyrixs-vouch-thread/page-69#post-43689319">
-                            <img src={AssassinName} alt="AssassinReview" /><img src={AssassinDonorRank} alt="AssassinDonorRank" />
-                        </a>                              
-                        <Card>
-                            <Card.Body>"Sold him another 500m 07 quick and easy thanks man!" (May 19, 2020)</Card.Body>
-                        </Card>
-                        </p>
-                    </div>
-            </div>
-        </div>
+                    </Col>                    
+                ))}
+            </Row>
+        </Container>
     )
 }
