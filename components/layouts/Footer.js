497d12ef8d014c285e2238a74586e76c584498b7
Jesse Cheung <18372099+dob-j@users.noreply.github.com>
2020-10-17 18:22:39 -0700

    working on footer cms part

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -9,43 +9,29 @@ import { Container, Row, Col} from 'react-bootstrap';
 import { attributes } from '../../content/footercomponent.md'
 
 const Footer = () => {
-    const { text, icons } = attributes;
+    const { text, icons, Sections } = attributes;
     return(
         <div className="main-footer" style={{padding: '4px', paddingTop: '0px', backgroundColor: 'white'}}>
             <Container>
                 <Row>
-                    {/* Column1 */}
-                    <Col md={3}>
-                        <h4>RPGStock</h4>
-                        <ul className="list-unstyled">
-                            <li><a href="/">About us</a></li>
-                            <li><a href="/">Blog</a></li>
-                            <li><a href="/feedback">Feedback</a></li> 
-                        </ul>
-                    </Col>
-                    {/* Column2 */}
-                    <Col md={3}>
-                        <h4>Support</h4>
-                        <ul className="list-unstyled">
-                            <li><a href="/">Help Center</a></li>
-                            <li><a href="/paymentmethods">Payment Methods</a></li>
-                            <li><a href="/">Contact Us</a></li>
-                        </ul>
-                    </Col>
-                    {/* Column3 */}
-                    <Col md={3}>
-                        <h4>Legal</h4>
-                        <ul className="list-unstyled">
-                            <li><a href="/terms-of-service">Terms of Service</a></li>
-                            <li><a href="/privacy-policy">Privacy Policy</a></li>
-                        </ul>
-                    </Col>
-                    {/* Column4 */}
+                    {Sections && 
+                        Sections.map((section,k) => (
+                            <Col md={3}>
+                            <h4>{section.sectiontitle}</h4>
+                            <ul className="list-unstyled">
+                                {section && section.subsection.map((subsection) =>(
+                                    <li><a href={subsection.subsectionurl}>{subsection.subsectiontitle}</a></li>
+                                ))
+                                }
+                            </ul>
+                            </Col>
+                        ))
+                    }
                     <Col md={3}>
                         <h4>Payment Methods</h4>
                         <ul className="list-unstyled">
                             <li>
-                                <img src={CoinbaseLogo} alt="Coinbase Cryptocurrencies Accepted here" width="120px" height="50px"/>
+                                <img src={CoinbaseLogo} alt="Coinbase Cryptocurrencies Accepted here" width="80px" height="40px"/>
                                 <img src={PaypalAcceptedHere} alt="PayPal Accepted Here" width="80px" height="40px"/>
                                 <img src={BitcoinAcceptedHere} alt="Bitcoin Accepted Here" width="80px" height="40px"/>
                                 <br />
