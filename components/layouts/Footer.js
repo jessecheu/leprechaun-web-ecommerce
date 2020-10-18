a67c27e1d67b5c063d8c6dcf151dc0393f60aa25
Jesse Cheung <18372099+dob-j@users.noreply.github.com>
2020-10-17 18:44:10 -0700

    working on footer cms part

--- components/layouts/Footer.js
+++ components/layouts/Footer.js
@@ -9,7 +9,7 @@ import { Container, Row, Col} from 'react-bootstrap';
 import { attributes } from '../../content/footercomponent.md'
 
 const Footer = () => {
-    const { text, icons, Sections, imagesection } = attributes;
+    const { text, icons, Sections, imagesectiontitle, imagesection } = attributes;
     return(
         <div className="main-footer" style={{padding: '4px', paddingTop: '0px', backgroundColor: 'white'}}>
             <Container>
@@ -29,11 +29,11 @@ const Footer = () => {
                     }
                     <Col md={3}>
 
-                        <h4>Payment Methods</h4>
+                        <h4>{imagesectiontitle}</h4>
                         {imagesection &&
                             imagesection.map((imagesection, k) => (
                                 <li className="list-unstyled">
-                                    <img src={imagesection.images.image} alt={imagesection.images.alttext} width="80px" height="40px"/>
+                                    <img src={imagesection.image} alt={imagesection.alttext} width="80px" height="40px"/>
                                 </li>
                             ))
                         }
