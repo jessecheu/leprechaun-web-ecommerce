ee00c85ba002d14af09f9acaaf43c1e43c58997c
Jesse Cheung <18372099+dob-j@users.noreply.github.com>
2020-10-29 19:34:51 -0700

    added sitemap.xml generator

---
+++ sitemap-generator.js
@@ -0,0 +1,17 @@
+  const { configureSitemap } = require('@sergeymyssak/nextjs-sitemap');
+
+const Sitemap = configureSitemap({
+  baseUrl: 'https://rpgstock.com',
+  exclude: ['/public/admin/*'],
+  excludeIndex: true,
+  pagesConfig: {
+    '/pages/*': {
+      priority: '0.5',
+      changefreq: 'daily',
+    },
+  },
+  isTrailingSlashRequired: true,
+  targetDirectory: __dirname + '/public',
+  pagesDirectory: __dirname + '/pages',
+});
+Sitemap.generateSitemap();
